57aaa4b9da643913bf94c546be3e364b52da0e7e
Jared Williams <accounts@jaredwilliams.com.au>
2023-10-19 19:35:28 +1000

    Fix generate query button

--- src/containers/view-species-category/index.tsx
+++ src/containers/view-species-category/index.tsx
@@ -117,7 +117,7 @@ const View = () => {
           urlWithPageNumberVar={routes.viewSpeciesCategoryWithVarAndPageNumberVar
             .replace(':speciesIdOrSlug', speciesId)
             .replace(':categoryName', categoryName)}
-          getQueryString={() => `species:${species.id}`}>
+          getQueryString={() => `species:${species.pluralname}`}>
           <Renderer />
         </PaginatedView>
       </div>
