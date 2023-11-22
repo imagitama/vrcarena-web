d98159a17d23e814cff8fb2b322ffe68d07405df
Jared Williams <accounts@jaredwilliams.com.au>
2023-11-22 21:21:47 +1000

    More icons

--- src/icons.ts
+++ src/icons.ts
@@ -8,9 +8,10 @@ export const Accessibility = React.lazy(() => import('@material-ui/icons/Accessi
 export const Brush = React.lazy(() => import('@material-ui/icons/Brush')) // textures
 export const Loyalty = React.lazy(() => import('@material-ui/icons/Loyalty')) // NSFW
 export const Visibility = React.lazy(() => import('@material-ui/icons/Visibility')) // eye textures / tracking / etc
+export const Link = React.lazy(() => import('@material-ui/icons/Link')) // requires linked asset
+export const Tune = React.lazy(() => import('@material-ui/icons/Tune')) // body sliders
 
 export const Copy = React.lazy(() => import('@emotion-icons/entypo/Copy').then(result => ({ default: result.Copy }))) // prefabs
-export const Bone = React.lazy(() => import('@emotion-icons/boxicons-solid/Bone').then(result => ({ default: result.Bone }))) // rigged/physbones
 export const HappyFace = React.lazy(() => import('@emotion-icons/material/TagFaces').then(result => ({ default: result.TagFaces }))) // custom gestures
 export const Map = React.lazy(() => import('@emotion-icons/bootstrap/Map').then(result => ({ default: result.Map }))) // uvmapped
 export const FemaleSign = React.lazy(() => import('@emotion-icons/boxicons-regular/FemaleSign').then(result => ({ default: result.FemaleSign })))
@@ -19,8 +20,10 @@ export const Tshirt = React.lazy(() => import('@emotion-icons/fa-solid/Tshirt').
 
 export const SubstancePainter = React.lazy(() => import('./assets/images/icons/substancepainter.svg').then(result => ({ default: result.ReactComponent })))
 export const Blender = React.lazy(() => import('./assets/images/icons/blender.svg').then(result => ({ default: result.ReactComponent })))
+export const Bone = React.lazy(() => import('./assets/images/icons/bone.svg').then(result => ({ default: result.ReactComponent })))
 export const Booth = React.lazy(() => import('./assets/images/icons/booth.svg').then(result => ({ default: result.ReactComponent })))
 export const ChilloutVR = React.lazy(() => import('./assets/images/icons/chilloutvr.svg').then(result => ({ default: result.ReactComponent })))
+export const Dancing = React.lazy(() => import('./assets/images/icons/dancing.svg').then(result => ({ default: result.ReactComponent })))
 export const Discord = React.lazy(() => import('./assets/images/icons/discord.svg').then(result => ({ default: result.ReactComponent })))
 export const FileFbx = React.lazy(() => import('./assets/images/icons/file_fbx.svg').then(result => ({ default: result.ReactComponent })))
 export const Fur = React.lazy(() => import('./assets/images/icons/fur.svg').then(result => ({ default: result.ReactComponent })))
@@ -30,6 +33,7 @@ export const Oculus = React.lazy(() => import('./assets/images/icons/oculus.svg'
 export const Patreon = React.lazy(() => import('./assets/images/icons/patreon.svg').then(result => ({ default: result.ReactComponent })))
 export const Photoshop = React.lazy(() => import('./assets/images/icons/photoshop.svg').then(result => ({ default: result.ReactComponent })))
 export const Poiyomi = React.lazy(() => import('./assets/images/icons/poiyomi.svg').then(result => ({ default: result.ReactComponent })))
+export const Puppet = React.lazy(() => import('./assets/images/icons/puppet.svg').then(result => ({ default: result.ReactComponent })))
 export const Twitch = React.lazy(() => import('./assets/images/icons/twitch.svg').then(result => ({ default: result.ReactComponent })))
 export const Unity = React.lazy(() => import('./assets/images/icons/unity.svg').then(result => ({ default: result.ReactComponent })))
 export const Under18 = React.lazy(() => import('./assets/images/icons/under18.svg').then(result => ({ default: result.ReactComponent }))) // nsfw_included
