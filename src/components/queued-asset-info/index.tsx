b8f2e0b5acb964a8fb50384ce955612f8379db69
Jared Williams <accounts@jaredwilliams.com.au>
2024-09-16 12:52:34 +0800

    Allow decline options

--- src/components/queued-asset-info/index.tsx
+++ src/components/queued-asset-info/index.tsx
@@ -35,6 +35,7 @@ const QueuedAssetInfo = ({
           existingEditorNotes={asset.editornotes}
           onDone={hydrate}
           showBox={false}
+          allowDeclineOptions={true}
         />
       ) : null}
     </Message>
