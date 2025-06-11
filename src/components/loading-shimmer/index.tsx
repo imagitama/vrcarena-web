a67655037ce0d6ea68cf8819328ba02024c85c90
Jared Williams <accounts@jaredwilliams.com.au>
2025-06-11 17:48:49 +0800

    Fix shimmer

--- src/components/loading-shimmer/index.tsx
+++ src/components/loading-shimmer/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import { makeStyles } from '@mui/styles'
-import './skeleton-loading-styles.css'
 
 const useStyles = makeStyles({
+  '@global': {
+    '@keyframes shimmer': {
+      '0%': { transform: 'translateX(-100%)' },
+      '100%': { transform: 'translateX(100%)' },
+    },
+  },
   root: {
     width: '100%',
-    height: '100%',
+    height: '1em',
+    display: 'inline-block',
+    position: 'relative',
+    overflow: 'hidden',
+    backgroundColor: '#3b3b3b',
+    '&:after': {
+      content: '""',
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      transform: 'translateX(-100%)',
+      backgroundImage:
+        'linear-gradient(90deg, rgba(255, 255, 255, 0) 0, rgba(255, 255, 255, 0.1) 20%, rgba(255, 255, 255, 0.3) 60%, rgba(255, 255, 255, 0))',
+      animation: `shimmer 2s infinite`,
+    },
   },
 })
 
@@ -17,7 +38,5 @@ export default ({
   height?: number | string
 }) => {
   const classes = useStyles()
-  return (
-    <div className={`skeleton-box ${classes.root}`} style={{ width, height }} />
-  )
+  return <div className={classes.root} style={{ width, height }} />
 }
