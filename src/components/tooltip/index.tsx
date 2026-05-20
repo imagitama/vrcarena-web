56a7baf16c66a9a373cf87e9a84ca4552e83a0df
Jared Williams <accounts@jaredwilliams.com.au>
2026-05-20 08:37:58 +0800

    Tweak delays on touch

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 import MaterialTooltip, { TooltipProps } from '@mui/material/Tooltip'
 
 const Tooltip = (props: TooltipProps) => (
-  <MaterialTooltip arrow placement="top" {...props} />
+  <MaterialTooltip
+    arrow
+    placement="top"
+    enterTouchDelay={300}
+    leaveTouchDelay={2000}
+    {...props}
+  />
 )
 
 export default Tooltip
