0f62fc05a64b8978919b478aa6c5734e3d6db0ec
Jared Williams <accounts@jaredwilliams.com.au>
2026-08-05 09:19:17 +0800

    Better error

--- src/components/error-boundary-message/index.tsx
+++ src/components/error-boundary-message/index.tsx
@@ -3,27 +3,37 @@ import ErrorMessage, { ErrorMessageProps } from '@/components/error-message'
 import { DISCORD_URL, EMAIL } from '@/config'
 import { base64EncodeString } from '@/utils'
 
+const getIsRefreshableError = (error: Error) =>
+  error.message.includes('error loading dynamically imported module')
+const reloadPage = () => window.location.reload()
+
 const ErrorBoundaryMessage = (
   props: { error: Error } & Omit<ErrorMessageProps, 'children'>
-) => (
-  <ErrorMessage
-    hintText={
-      <>
-        This doesn't usually happen. Please{' '}
-        <a href={DISCORD_URL}>join our Discord</a> or{' '}
-        <a href={`mailto:${EMAIL}`}>email us</a> to report this error so I can
-        fix it.
-        <br />
-        <br />
-        Error:{' '}
-        {props.error && props.error.message
-          ? base64EncodeString(props.error.message)
-          : '(no error)'}
-      </>
-    }
-    {...props}>
-    Something really screwed up (which doesn't happen often).
-  </ErrorMessage>
-)
+) =>
+  getIsRefreshableError(props.error) ? (
+    <ErrorMessage onOkay={reloadPage}>
+      The site has just received a patch to fix an issue or add a new feature.
+      You must refresh the page.
+    </ErrorMessage>
+  ) : (
+    <ErrorMessage
+      hintText={
+        <>
+          This doesn't usually happen. Please{' '}
+          <a href={DISCORD_URL}>join our Discord</a> or{' '}
+          <a href={`mailto:${EMAIL}`}>email us</a> to report this error so I can
+          fix it.
+          <br />
+          <br />
+          Error:{' '}
+          {props.error && props.error.message
+            ? base64EncodeString(props.error.message)
+            : '(no error)'}
+        </>
+      }
+      {...props}>
+      Something really screwed up (which doesn't happen often).
+    </ErrorMessage>
+  )
 
 export default ErrorBoundaryMessage
